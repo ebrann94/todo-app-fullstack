b82a61b2c4e2f6caa8fabef5f654185f3be1cf3a
Ethan Brann <ethan.brann94@gmail.com>
2019-03-26 11:59:40 +0000

    Rearranged file structure for deployment

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const userRouter = require('./routers/user');
 const todoRouter = require('./routers/task');
 
 // Server Setup
-const publicPath = path.join(__dirname, '..', 'frontend', 'public');
+const publicPath = path.join(__dirname, 'front-end', 'dist');
+// const publicPath = path.join(__dirname, 'public');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -16,7 +17,8 @@ app.use(express.json());
 app.use((req, res, next) => {
     console.log('Request received', req.url);
     next();
-})
+});
+app.use(express.static(publicPath));
 app.use('/blah', userRouter);
 app.use('/blah', todoRouter);
 // app.use(express.static(publicPath));
